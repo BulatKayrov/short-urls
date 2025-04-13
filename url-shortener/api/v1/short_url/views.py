11f6147813a6134e85f8e7ca6938d36815ce4cad
BulatKayrov <bulat.kajrov@yandex.ru>
2025-04-13 17:49:30 +0400

    add form fields for view function

--- url-shortener/api/v1/short_url/views.py
+++ url-shortener/api/v1/short_url/views.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter, Depends
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status, Form
+from pydantic import AnyHttpUrl
 from starlette.responses import RedirectResponse
 
 from api.v1.short_url.dependencies import SHORT_URLS, prefetch_short_urls
@@ -15,3 +18,17 @@ async def short_url():
 @router.get("/short-url/{slug}")
 async def redirect_short_url(url=Depends(prefetch_short_urls)):
     return RedirectResponse(url.target_url)
+
+
+@router.post(
+    path="/short-url",
+    # response_model=ShortUrl,
+    status_code=status.HTTP_201_CREATED,
+    summary="Create new short url",
+    description="Create a new short url",
+)
+async def create_short_url(
+    target_url: Annotated[AnyHttpUrl, Form()],
+    slug: Annotated[str, Form(min_length=5, max_length=100)],
+):
+    return {"data": ShortUrl(target_url=target_url, slug=slug), "status": 201}
