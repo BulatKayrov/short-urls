6f178fdd12a0df3dfbb0f3346c39713884a26220
BulatKayrov <bulat.kajrov@yandex.ru>
2025-04-20 11:43:03 +0400

    unsafe methods

--- url-shortener/api/v1/short_url/dependencies.py
+++ url-shortener/api/v1/short_url/dependencies.py
@@ -1,11 +1,13 @@
 from logging import getLogger
 
-from fastapi import HTTPException, status, BackgroundTasks
+from fastapi import HTTPException, status, BackgroundTasks, Request
 
 from api.v1.short_url.crud import storage
 from api.v1.short_url.schemas import ShortUrl
 
 logger = getLogger(__name__)
+
+UNSAFE_METHODS = frozenset({'POST', 'PUT', 'PATCH', 'DELETE'})
 
 
 def prefetch_short_urls(slug: str):
@@ -15,7 +17,9 @@ def prefetch_short_urls(slug: str):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
 
-def save_storage_state(bg_task: BackgroundTasks):
+def save_storage_state(bg_task: BackgroundTasks, request: Request):
+    logger.info(request.method)
     yield
-    logger.info("Saving storage state")
-    bg_task.add_task(storage.save())
+    if request.method not in UNSAFE_METHODS:
+        logger.info("Saving storage state")
+        bg_task.add_task(storage.save())
